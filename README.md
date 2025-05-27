42194a8b45e602dd46b86daf1ab06d8d84c59d8a
jessesillman <jesse.sillman@yahoo.com>
2025-05-27 12:34:17 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Weather App 🌦️
+
+I implemented a simple weather application using `OpenWeatherMap API` to fetch real-time weather data
+based on user input. The application includes a `Flask` backend defined in `app.py`, a frontend built with `templates/index.html` using `Bootstrap` for styling and `Dockerfile` for containerized deployment, and a `requirements.txt` file to manage Python dependencies.
+
+# Usage
+
+You're welcome to use and change the code in any way you like. Have fun building! 🤖
---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+app = Flask(__name__)
+
+def get_weather(api_key, city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city,
+        'appid': api_key,
+        'units': 'metric'  
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if response.status_code == 200:
+            return data
+        else:
+            return None
+
+    except Exception as e:
+        return None
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        city = request.form['city']
+        api_key = os.getenv("API_KEY")
+        weather_data = get_weather(api_key, city)
+        return render_template('index.html', weather_data=weather_data)
+    
+    return render_template('index.html', weather_data=None)
+
+if __name__ == "__main__":
+    app.run(port=int(os.environ.get("PORT", 8081)), host='0.0.0.0', debug=True)
